82e904130733f370814d4aeb96da7857bf58c447
Mahipatel7 <145039790+Mahipatel7@users.noreply.github.com>
2024-08-09 23:17:16 -0400

    Delete Assignment2 directory

--- Assignment2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module georgiancollegecomp1011.assignment2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens georgiancollegecomp1011.assignment2 to javafx.fxml;
-    exports georgiancollegecomp1011.assignment2;
-}
